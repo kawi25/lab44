f338ecc1c10db290f00d327c630fa84d0a3da2db
FRANCISCO GABRIEL CHICAS BONILLA <SMSS117822@ugb.edu.sv>
2022-08-26 20:53:00 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace lab44
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Hashtable lista = new Hashtable(),
+          Console.Writeline(lista.count);
+
+            for (int i = 0; i < 10; i++)
+            {
+                lista.Add(i, "Dato" + i);
+            }
+            foreach (int key in lista.keys)
+            {
+                Console.Writeline("clave: {0} === valor: {1}", key, lista[key]);
+            }
+        }
+    }
+}
